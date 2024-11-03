030ac5cb1899e6c76954e879f72dfae126cdb8d1
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-11-03 09:13:19 +0630

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,18 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import router from "@/router";
+
+export default {
+  setup() {
+    router.beforeEach((to, from, next) => {
+      if (to.name == "products" || to.name == "navBtn") {
+        next();
+      }
+    });
+    return {};
+  },
+};
+</script>
